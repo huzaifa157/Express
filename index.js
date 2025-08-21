e19ec50c6bbc072a7b3194b6cf266e271518ea11
huzaifa157 <m.huzaifa157@gmail.com>
2025-08-21 17:50:21 +0500

    ✨ Learn Express basics: routes, requests & responses
    - Created basic routes using Express
    - Handled GET requests and sent HTML responses
    - Practiced working with query strings using

--- index.js
+++ index.js
@@ -15,12 +15,9 @@
 
 // Using middleware (functions that run between request and response, e.g., logging, authentication).
 
+import express from "express"; // Importing express
 
-
-
-import express from 'express'   // Importing express
-
-const app = express()           // Creating an express application
+const app = express(); // Creating an express application
 
 // When someone goes to "http://localhost:3000/"
 
@@ -28,16 +25,16 @@ const app = express()           // Creating an express application
 
 // Used for specific routes with the GET request (like opening a webpage in a browser).
 // Runs only when the URL path matches exactly.
-                                     
-app.get("/",(req,res)=>{                                  // Send back response
-    res.send("you contacted the home path")
-})
 
-app.get("/about",(req,res)=>{
-    res.send("you contacted the about path")
-})
+app.get("/", (req, res) => {
+  // Send back response
+  res.send("you contacted the home path");
+});
 
- 
+app.get("/about", (req, res) => {
+  res.send("you contacted the about path");
+});
+
 // 🔹 What is a Path Parameter?        (also called route parameters)
 
 // A path parameter is a variable part of the URL.
@@ -47,12 +44,12 @@ app.get("/about",(req,res)=>{
 // app.get("/:username/:id", (req,res)=>{
 
 //  let {username , id} = req.params;
-//  res.send(`Welcome to the page of @${username}.Page id is #${id}`)   
+//  res.send(`Welcome to the page of @${username}.Page id is #${id}`)
 // })
 
-// for html format 
+// for html format
 
-app.get("/:username/:id", (req,res) => {
+app.get("/:username/:id", (req, res) => {
   let { username, id } = req.params;
 
   let htmlstr = `
@@ -63,25 +60,35 @@ app.get("/:username/:id", (req,res) => {
   res.send(htmlstr);
 });
 
+// query Strings
 
+// How it works
+// Query parameters are the part of a URL that comes after a ?.
+// They’re written as key=value pairs.
 
+app.get("/search", (req, res) => {
+  let { q } = req.query;
+
+  if (!q) {
+    res.send(`Nothing search`);
+  }
+  res.send(`<h1>Search result for Query: ${q}</h1>`);
+});
 
 // app.use()
 
 // Used to add middleware or functions that run for every request (all routes), unless you specify a path.
 // Can work for all request types (GET, POST, etc.)
 
-app.use((req, res) => {                /*all random request other than mentioned path in get */
-  res.send('this path is not valid. Something is missing or path is invalid')       
-})
-
-
+app.use((req, res) => {
+  /*all random request other than mentioned path in get */
+  res.send("this path is not valid. Something is missing or path is invalid");
+});
 
 // Start server on port 3000
-app.listen(3000, () => {        
-  console.log("Server running at http://localhost:3000")
-})
-
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
 
 // What is nodemon?
 
