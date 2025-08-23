9eafa9c7a1d34151df22cd386fce6a6d3d991406
huzaifa157 <m.huzaifa157@gmail.com>
2025-08-23 22:07:19 +0500

    use of middleware to parse data for post req

--- Micellaneous/Backend/index.js
+++ Micellaneous/Backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+app.use(express.urlencoded({extended: true})) //  Middleware to parse form data (req.body) when seding post request 
+// extended: true => allows nested objects in form data 
+
 
 app.get("/register", (req, res) => {
     let {user , password} = req.query;
@@ -9,7 +12,12 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    res.send("Its Post response");
+
+    let {user , password} = req.body;
+     res.send(`Its Get response Welcome dear ${user}`);
+
+    res.send("It's Post response");
+    console.log(req.body)
 });
 
 
