4f670dac93797d5e65a435baf63a6b715270b72c
huzaifa157 <m.huzaifa157@gmail.com>
2025-08-23 19:29:22 +0500

    saving static files

--- EJS/index.js
+++ EJS/index.js
@@ -32,14 +32,23 @@ const path = require("path")
 const port = 8080;
 
 app.set("view engine", "ejs");
-
-
 // This line tells Express where to find our EJS templates.
 // By default, Express looks inside a folder named "views" in the root directory.
 // path.join(__dirname, "views") makes sure Express always points to the correct "views" folder,
 // no matter where the project is run from.either it is runing from root or from main folder it will run after it .
 // to use path.join we have to require it 
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// What does express.static() do?
+
+// In an Express.js app, you usually want to serve files like: HTML ,CSS ,JavaScript (frontend) ,Images
+
+// These files don’t change on the server — they are called static files.
+// 👉 express.static() is a built-in middleware in Express that tells your app where to look for these static files. it looks on public folder
+
+// path.join(__dirname, "public") → makes sure Express always finds the folder correctly, so we can also run project from root 
+//  no matter which operating system you’re using (Windows, Mac, Linux).
 
 
 
@@ -57,17 +66,15 @@ app.get("/ludo", (req,res)=>{
 
 
 app.get("/instagram/:username", (req, res) => {
-    
-let {username} = req.params;
-const instaData = require("./data.json")
+  let { username } = req.params;
+  const instaData = require("./data.json");
 
-let data = instaData[username]
- if(data){
-      res.render("instagram" , {data});
- } else{
-    res.render("err")
- }
-
+  let data = instaData[username];
+  if (data) {
+    res.render("instagram", { data });
+  } else {
+    res.render("err");
+  }
 });
 
 
