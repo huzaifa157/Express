7bc8011e509b99b4bbdf4851643f145bbf7f616f
huzaifa157 <m.huzaifa157@gmail.com>
2025-08-25 03:21:20 +0500

    complete rest api uses

--- Rest_Class/tempCodeRunnerFile.js
+++ Rest_Class/tempCodeRunnerFile.js
@@ -93,11 +93,7 @@ app.patch("/posts/:id", (req, res) => {
 });
 
 
-app.delete("/posts/:id", (req, res) => {
-    let { id } = req.params;
-    posts = posts.filter((p) => id !== p.id);
-    res.redirect("/posts");
-});
+
 
 
 
